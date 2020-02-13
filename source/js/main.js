fddee526f7516a79231ea17fda77c75413fc6d19
leo <leofallensoul@gmail.com>
2020-02-13 22:29:35 +0300

    дополняет хедеру адаптивность по всем брейкпоинтам

--- source/js/main.js
+++ source/js/main.js
@@ -1,2 +1,11 @@
+const promoButton = document.querySelector(`.promo__button`);
+const TABLET_WIDTH = 768;
 
 
+window.onresize = (evt) => {
+  if (evt.target.innerWidth < TABLET_WIDTH) {
+    promoButton.textContent = `БЕСПЛАТНАЯ КОНСУЛЬТАЦИЯ`;
+  } else {
+    promoButton.textContent = `ПОЛУЧИТЬ БЕСПЛАТНУЮ КОНСУЛЬТАЦИЮ`;
+  }
+}
