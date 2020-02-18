bd845d312fc13d0ced77150120c3ec0319426f96
leo <leofallensoul@gmail.com>
2020-02-19 01:53:29 +0300

    добавляет попап обратной связи и блок feedback

--- build/js/main.js
+++ build/js/main.js
@@ -7,6 +7,37 @@ var sections = document.querySelector(".page-footer__sections").querySelector("u
 var accordionSection = sectionsButton.querySelector(".accordion");
 var accordionAdres = adresButton.querySelector(".accordion");
 var tabletWidth = 768;
+var scrollDown = document.querySelector(".promo__scroll-down");
+var scrolСonsultation = document.querySelector(".promo__button");
+var feedbackBlock = document.querySelector("#feedback");
+var advantagesBlock = document.querySelector("#advantages");
+var callBackButton = document.querySelector(".page-header__contacts-call");
+var callPopap = document.querySelector(".call-popap");
+var popapCloseButton = callPopap.querySelector(".call-popap__close-button");
+var popapOverlay = callPopap.querySelector(".call-popap__overlay");
+var popapSubmitButton = callPopap.querySelector("button[type=submit]");
+var popapInputName = callPopap.querySelector("input[name=name]");
+var popapInputTel = callPopap.querySelector("input[name=tel]");
+var popapInputQuest = callPopap.querySelector("textarea");
+
+var localStorageCheck = function localStorageCheck() {
+  if (localStorage.getItem('name') !== null) {
+    popapInputName.value = localStorage.getItem('name');
+  }
+
+  if (localStorage.getItem('tel') !== null) {
+    popapInputTel.value = localStorage.getItem('tel');
+  }
+
+  if (localStorage.getItem('quest') !== null) {
+    popapInputQuest.value = localStorage.getItem('quest');
+  }
+};
+
+var KEYS = {
+  ESC: 27,
+  ENTER: 13
+};
 
 var accordion = function accordion(target) {
   if (target == sectionsButton) {
@@ -30,9 +61,102 @@ var accordionListener = function accordionListener(arr) {
 
 accordionListener([sectionsButton, adresButton]);
 
+if (scrolСonsultation) {
+  scrolСonsultation.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === KEYS.ENTER) {
+      evt.preventDefault();
+      feedbackBlock.scrollIntoView({
+        behavior: "smooth"
+      });
+    }
+  });
+  scrolСonsultation.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    feedbackBlock.scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+}
+
+if (scrollDown) {
+  scrollDown.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === KEYS.ENTER) {
+      evt.preventDefault();
+      advantagesBlock.scrollIntoView({
+        behavior: "smooth"
+      });
+    }
+  });
+  scrollDown.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    advantagesBlock.scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+}
+
+var popapClose = function popapClose() {
+  callPopap.classList.remove("call-popap__open");
+};
+
+var onEscPress = function onEscPress(evt) {
+  if (evt.keyCode === KEYS.ESC) {
+    evt.preventDefault();
+    popapClose();
+  }
+};
+
+var inputFocusCheck = function inputFocusCheck() {
+  [popapInputName, popapInputTel, popapInputQuest].forEach(function (input) {
+    input.addEventListener("focusin", function () {
+      document.removeEventListener("keydown", onEscPress);
+    });
+    input.addEventListener("focusout", function () {
+      document.addEventListener("keydown", onEscPress);
+    });
+  });
+};
+
+var popapOpen = function popapOpen(evt) {
+  inputFocusCheck();
+  localStorageCheck();
+  evt.preventDefault();
+  callPopap.classList.add("call-popap__open");
+  document.addEventListener("keydown", onEscPress);
+  document.addEventListener("click", function (evt) {
+    if (evt.target === popapOverlay) {
+      popapClose();
+    }
+  });
+};
+
+if (popapSubmitButton) {
+  popapSubmitButton.addEventListener("click", function () {
+    if (popapInputName.value) {
+      localStorage.setItem('name', popapInputName.value);
+    }
+
+    if (popapInputTel.value) {
+      localStorage.setItem('tel', popapInputTel.value);
+    }
+
+    if (popapInputQuest.value) {
+      localStorage.setItem('quest', popapInputQuest.value);
+    }
+  });
+}
+
+if (callBackButton) {
+  callBackButton.addEventListener("click", popapOpen);
+}
+
+if (popapCloseButton) {
+  popapCloseButton.addEventListener("click", popapClose);
+}
+
 window.onresize = function (evt) {
   if (evt.target.innerWidth > tabletWidth) {
-    adres.classList.remove("hidden");
-    sections.classList.remove("hidden");
+    adres.classList.remove("page-footer__list--hidden");
+    sections.classList.remove("page-footer__list--hidden");
   }
 };
--- source/js/main.js
+++ source/js/main.js
@@ -7,6 +7,35 @@ const sections = document.querySelector(`.page-footer__sections`).querySelector(
 const accordionSection = sectionsButton.querySelector(`.accordion`);
 const accordionAdres = adresButton.querySelector(`.accordion`);
 const tabletWidth = 768;
+const scrollDown = document.querySelector(`.promo__scroll-down`);
+const scrolСonsultation = document.querySelector(`.promo__button`);
+const feedbackBlock = document.querySelector(`#feedback`);
+const advantagesBlock = document.querySelector(`#advantages`);
+const callBackButton = document.querySelector(`.page-header__contacts-call`);
+const callPopap = document.querySelector(`.call-popap`);
+const popapCloseButton = callPopap.querySelector(`.call-popap__close-button`);
+const popapOverlay = callPopap.querySelector(`.call-popap__overlay`);
+const popapSubmitButton = callPopap.querySelector(`button[type=submit]`);
+const popapInputName = callPopap.querySelector(`input[name=name]`);
+const popapInputTel = callPopap.querySelector(`input[name=tel]`);
+const popapInputQuest = callPopap.querySelector(`textarea`);
+
+const localStorageCheck = () => {
+  if (localStorage.getItem('name') !== null) {
+    popapInputName.value = localStorage.getItem('name');
+  }
+  if (localStorage.getItem('tel') !== null) {
+    popapInputTel.value = localStorage.getItem('tel');
+  }
+  if (localStorage.getItem('quest') !== null) {
+    popapInputQuest.value = localStorage.getItem('quest');
+  }
+}
+
+const KEYS = {
+  ESC: 27,
+  ENTER: 13
+}
 
 const accordion = (target) => {
   if (target == sectionsButton) {
@@ -30,13 +59,105 @@ const accordionListener = (arr) => {
 
 accordionListener([sectionsButton, adresButton]);
 
+if (scrolСonsultation) {
+  scrolСonsultation.addEventListener(`keydown`, (evt) => {
+    if (evt.keyCode === KEYS.ENTER) {
+      evt.preventDefault();
+      feedbackBlock.scrollIntoView({
+        behavior: "smooth"
+    });
+   }
+  });
+
+
+  scrolСonsultation.addEventListener(`click`, (evt) => {
+    evt.preventDefault();
+    feedbackBlock.scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+}
+
+
+if (scrollDown) {
+  scrollDown.addEventListener(`keydown`, (evt) => {
+    if (evt.keyCode === KEYS.ENTER) {
+      evt.preventDefault();
+      advantagesBlock.scrollIntoView({
+        behavior: "smooth"
+    });
+   }
+  });
+
+  scrollDown.addEventListener(`click`, (evt) => {
+    evt.preventDefault();
+    advantagesBlock.scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+}
+
+
+const popapClose = () => {
+  callPopap.classList.remove(`call-popap__open`);
+}
+
+const onEscPress = (evt) => {
+    if (evt.keyCode === KEYS.ESC) {
+      evt.preventDefault();
+      popapClose();
+    }
+};
+
+const inputFocusCheck = () => {
+  [popapInputName, popapInputTel, popapInputQuest].forEach(input => {
+    input.addEventListener(`focusin`, () => {
+      document.removeEventListener(`keydown`, onEscPress);
+    });
+    input.addEventListener(`focusout`, () => {
+      document.addEventListener(`keydown`, onEscPress);
+    })
+  });
+};
+
+const popapOpen = (evt) => {
+  inputFocusCheck();
+  localStorageCheck();
+  evt.preventDefault();
+  callPopap.classList.add(`call-popap__open`);
+  document.addEventListener(`keydown`, onEscPress);
+  document.addEventListener(`click`, (evt) => {
+    if (evt.target === popapOverlay) {
+      popapClose();
+    }
+  })
+};
+
+if (popapSubmitButton) {
+  popapSubmitButton.addEventListener(`click`, () => {
+    if (popapInputName.value) {
+      localStorage.setItem('name', popapInputName.value);
+    }
+    if (popapInputTel.value) {
+      localStorage.setItem('tel', popapInputTel.value);
+    }
+    if (popapInputQuest.value) {
+      localStorage.setItem('quest', popapInputQuest.value);
+    }
+  });
+}
+
+if (callBackButton) {
+  callBackButton.addEventListener(`click`, popapOpen);
+}
+
+if (popapCloseButton) {
+  popapCloseButton.addEventListener(`click`, popapClose);
+}
 
 window.onresize = (evt) => {
   if (evt.target.innerWidth > tabletWidth) {
-    adres.classList.remove(`hidden`);
-    sections.classList.remove(`hidden`);
+    adres.classList.remove(`page-footer__list--hidden`);
+    sections.classList.remove(`page-footer__list--hidden`);
   }
 };
-
-
-
